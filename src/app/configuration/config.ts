0f2e60f496a173ecc36f8c1eec8fed04405db447
aditi1009 <aditi@tankup.co.in>
2022-01-03 15:20:18 +0530

    .

--- src/app/configuration/config.ts
+++ src/app/configuration/config.ts
@@ -2,7 +2,7 @@
 export class Config {
   // static API_URL = 'http://3.109.101.84:9000/'; //staging
   // static API_URL = 'http://localhost:3000/'; //prod
-  static API_URL = 'http://192.168.0.182:3000/'; //local
+  static API_URL = 'http://192.168.102.165:3000/'; //local
   // static API_URL_LOCK = 'http://15.206.96.109:3100/api/automation'
   static makeCall(number : number){
     let location ='tel:+91'+number+'';
